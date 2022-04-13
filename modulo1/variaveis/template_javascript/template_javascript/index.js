087d79640df519ff83578d06d491a3f08ac0cff4
Jessica Oliveira Braga <jessi-oliveira@hotmail.com>
2022-04-12 23:26:50 -0300

    variaveis

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,38 @@
+/* Analise 1
+console.log(b)= 10
+console.log(a, b)= 10, 5
+
+Analise 2
+console.log(a, b, c)= 20, 10, 10
+
+Analise 3
+cargaHoraria
+valorDiario
+Calcula o valor do recebido por hora de trabalho
+*/
+
+// Questão 1 
+let nome= prompt("Qual é o seu nome?")
+let idade= prompt("Qual é a sua idade?")
+console.log (typeof nome, typeof idade )
+// informação não atribuida
+// Mudança de undefinid para string string
+console.log("Olá",nome , "você tem", idade, "anos")
+
+//Questão 2
+let roupa= prompt ("você está usando roupa azul hoje?")
+let vestido= prompt ("Você está usando vestido hoje?")
+let saltoAlto= prompt ("você está usando salto alto hoje?")
+console.log("você está usando roupa azul hoje?", roupa)
+console.log("você está usando vestido hoje?", vestido)
+console.log("vocêestá usando salto alto hoje?", saltoAlto)
+
+//Questão 3
+let a= 10
+let b= 25
+c= a
+a= b
+b= c
+console.log("o novo valor de a é", a)
+console.log("o novo valor de b é", b)
+
