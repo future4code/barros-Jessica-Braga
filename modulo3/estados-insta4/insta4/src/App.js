504eb6161c581a03f5b30602c3d07abc2905878f
Jessica Oliveira Braga <jessi-oliveira@hotmail.com>
2022-06-28 22:01:48 -0300

    atividade-estados-insta4

---
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Post from './components/Post/Post';
+import './style.css'
+
+
+function App() {
+return(
+  <div className='MainContainer'>
+           <Post
+            nomeUsuario={'paulinha'}
+            fotoUsuario={'https://picsum.photos/50/50'}
+            fotoPost={'https://picsum.photos/200/150'}
+          />
+
+<Post
+            nomeUsuario={'Robert'}
+            fotoUsuario={'https://picsum.photos/50/51'}
+            fotoPost={'https://picsum.photos/200/151'}
+          />
+
+<Post
+            nomeUsuario={'James'}
+            fotoUsuario={'https://picsum.photos/50/52'}
+            fotoPost={'https://picsum.photos/200/152'}
+          />
+  </div>
+
+  
+)
+
+}
+
+
+export default App;
