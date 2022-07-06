f57da4fba5a6e316980b2566e020ca95fce48d01
Jessica Oliveira Braga <jessi-oliveira@hotmail.com>
2022-07-05 21:25:03 -0300

    continuidade-insta4

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import Post from './components/Post/Post';
 import './style.css'
-
+import {MainContainer} from './components/Post/style.js';
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
+const listaPost = [
 
-<Post
-            nomeUsuario={'Robert'}
-            fotoUsuario={'https://picsum.photos/50/51'}
-            fotoPost={'https://picsum.photos/200/151'}
-          />
+  {nomeUsuario:'paulinha',
+  fotoUsuario:'https://picsum.photos/50/50',
+  fotoPost:'https://picsum.photos/200/150'},
 
-<Post
-            nomeUsuario={'James'}
-            fotoUsuario={'https://picsum.photos/50/52'}
-            fotoPost={'https://picsum.photos/200/152'}
-          />
-  </div>
+  {nomeUsuario:'Robert',
+  fotoUsuario:'https://picsum.photos/50/51',
+  fotoPost:'https://picsum.photos/200/151'},
 
-  
-)
+  {nomeUsuario:'James',
+  fotoUsuario:'https://picsum.photos/50/52',
+  fotoPost:'https://picsum.photos/200/152'},
+]
 
-}
+const componentePost = listaPost.map ((valor, index) => {
+return <li key={index}><Post nomeUsuario={valor.nomeUsuario} fotoUsuario=
+{valor.fotoUsuario} fotoPost={valor.fotoPost}/></li>
+})
 
+console.log (listaPost)
+
+return (
+  <MainContainer>
+    <ul> {componentePost}</ul>
+  </MainContainer>
+)}
 
 export default App;
